5b573c6b79b68cc7199a07aa6e9aee389fc78ef3
Marjorie <marjorie.crtz@yahoo.com>
2024-02-08 11:27:39 +0800

    Update responsive design and fix about section

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const searchBox = document.querySelector('.search-box');
 const searchIcon = document.querySelector('#search-icon');
 
+// Search Icon
 searchIcon.addEventListener('click', function () {
   searchBox.classList.toggle('active');
+  navbar.classList.remove('active');
 });
 
+// Remove search box and nav bar when scrolling
 window.onscroll = () => {
   searchBox.classList.remove('active');
+  navbar.classList.remove('active');
 };
 
-let header = document.querySelector('header');
+const header = document.querySelector('header');
 
+// Header
 window.addEventListener('scroll', function () {
   header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+// Menu Icon for responsive design
+const navbar = document.querySelector('.navbar');
+const menu = document.getElementById('menu-icon');
+
+menu.addEventListener('click', function () {
+  navbar.classList.toggle('active');
+  searchBox.classList.remove('active');
 });
