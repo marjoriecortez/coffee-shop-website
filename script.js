b5ceea15b8f19834da6ac2cc679c35e0e3e6dd70
Marjorie <marjorie.crtz@yahoo.com>
2024-02-08 09:14:35 +0800

    Update customer section

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const searchIcon = document.querySelector('#search-icon');
 searchIcon.addEventListener('click', function () {
   searchBox.classList.toggle('active');
 });
+
+window.onscroll = () => {
+  searchBox.classList.remove('active');
+};
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
